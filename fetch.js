5308e6ce3f499c65362ecf3951d9530436f2bbf7
Amine Tahiri <atahiri@student.1337.ma>
2021-04-17 17:29:25 +0000

    comment

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
 const fetch = require('node-fetch');
 const data = fetch('https://jsonplaceholder.typicode.com/todos/');
 data.then((response) => response.json()).then((final) => console.log(final));
-console.log('me first');
+// console.log('me first');
